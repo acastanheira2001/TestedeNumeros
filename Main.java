ee30d8da5018f0f8944e1833c87c5f4e2d334ef7
acastanheira2001 <acastanheira2001@yahoo.com.br>
2019-01-10 09:58:20 -0200

    Create Main.java
    Testando a insercao de codigo pela interface web para depois clonar o repositório.

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+		public static void main(String[] args) {
+				Scanner scan = new Scanner(System.in);
+				int i, j, NumOriginal;
+
+				System.out.println("digite um numero acima de 0");
+				NumOriginal = scan.nextInt();
+
+				for(j=NumOriginal; j>=1; j--)
+				{
+					for(i=j; i>=1; i--) {
+						System.out.print(i * i  + " ");
+
+					}
+					System.out.println("");
+				}
+			}   
+}
